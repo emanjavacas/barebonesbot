26067389e61a0700e15ab1c42b8de066593ed730
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-01-03 18:42:49 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -11,8 +11,11 @@ setup(
     version='0.1',
     description='A configurable Twitter-bot',
     author='Enrique Manjavacas',
-    license='MIT',
+    author_email='enrique.manjavacas@gmail.com',
+    url='https://www.github.com/emanjavacas/barebonesbot/',
+    download_url='https://api.github.com/repos/emanjavacas/barebonesbot/tarball',
     install_requires=[
         'birdy>=0.2'
-    ]
+    ],
+    license='MIT'
 )
